6b9335feae67d4c3f1b66826b2424f86660d2665
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-22 15:28:12 -0500

    Original exercise program

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include "PPP.h"
 
+double ctok(double c) {
+    int k = c + 273.25;
+    return int
+}
+
 int main()
 {
-    
+    double c = 0;
+    cin >> d;
+    double k = ctok("c");
+    Cout << k << '\n';
 }
 
