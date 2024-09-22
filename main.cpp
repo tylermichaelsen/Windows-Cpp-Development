9734a069f46adbc81f418e8c780bdafa8af982ae
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-22 18:53:29 -0500

    Implement exception handling

--- main.cpp
+++ main.cpp
@@ -7,11 +7,24 @@ double ctok(double c) {
 
 int main()
 {
-    double c = 0;
-    cin >> c;
-    if (c < -273.15)
-        error("Temperature is below absolute zero.\n");
-    double k = ctok(c);
-    cout << k << '\n';
+    try {
+        double c = 0;
+        cin >> c;
+        if (c < -273.15)
+            error("Temperature is below absolute zero.\n");
+        double k = ctok(c);
+        cout << k << '\n';
+    }
+
+    catch (exception& e) {
+        cerr << "error: " << e.what() << '\n';
+        return 1;
+    }
+
+    catch (...) {
+        cerr << "Unexpected error occurred.\n";
+        return 2;
+    }
+     
 }
 
