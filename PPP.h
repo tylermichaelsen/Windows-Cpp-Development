2363c71d201ab590d684deb0dd5ed01ec34b876b
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-09-22 15:24:39 -0500

    Add project files.

---
+++ PPP.h
@@ -0,0 +1,10 @@
+// PPP.h
+
+import PPP;
+using namespace PPP;
+using namespace std;
+
+// disgusting macro hack to guarantee range checking for []:
+#define vector Checked_vector
+#define span Checked_span
+
