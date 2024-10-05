b8f995c5a0e26faec82d422cb2d548e37fa7d3cd
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-10-05 02:54:43 -0500

    Loop the program until reaching an error

--- main.cpp
+++ main.cpp
@@ -8,23 +8,25 @@ double ctok(double c) {
 }
 
 int main()
-{
-    try {
-        double c = 0;
-        cin >> c;
-        double k = ctok(c);
-        cout << k << '\n';
-    }
+{  
+    while (true)
+    {
+        try {
+            double c = 0;
+            cin >> c;
+            double k = ctok(c);
+            cout << k << '\n';
+        }
 
-    catch (exception& e) {
-        cerr << "error: " << e.what() << '\n';
-        return 1;
-    }
+        catch (exception& e) {
+            cerr << "error: " << e.what() << '\n';
+            return 1;
+        }
 
-    catch (...) {
-        cerr << "Unexpected error occurred.\n";
-        return 2;
+        catch (...) {
+            cerr << "Unexpected error occurred.\n";
+            return 2;
+        }
     }
-     
 }
 
