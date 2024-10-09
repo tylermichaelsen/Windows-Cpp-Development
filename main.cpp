2aaee71b082aa03ec17751bfe1c7b5c4457ea109
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-10-09 04:51:02 -0500

    Create trys and catches

--- main.cpp
+++ main.cpp
@@ -6,7 +6,19 @@ int main()
 {
     bool keepRunning = true;
     while (keepRunning) {
+        try {
 
+        }
+
+        catch (exception& e) {
+            cerr << "Error occurred: " << e.what() << '\n';
+            return 1;
+        }
+
+        catch (...) {
+            cerr << "Unknown error occurred.\n";
+            return 2;
+        }
     }
     
 }
