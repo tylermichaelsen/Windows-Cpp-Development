2a1c399dd10579090da8611b0c433efb98071699
tylermichaelsen <tyler.michaelsen1@gmail.com>
2024-10-09 04:55:31 -0500

    Create DisplayMenu function

--- main.cpp
+++ main.cpp
@@ -2,6 +2,17 @@
 
 // Create a calculator with error handling and menu selection
 
+void DisplayMenu() {
+    cout << "Welcome to my C++ Calculator!\n\n" <<
+        "Please make a selection:\n" <<
+        "1. Addition\n" <<
+        "2. Subtraction\n" <<
+        "3. Multiplication\n" <<
+        "4. Division\n\n" <<
+        "Or press the '|' key to exit the program.\n";
+}
+
+
 int main()
 {
     bool keepRunning = true;
